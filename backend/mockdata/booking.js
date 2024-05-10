bd80c6051448c30c8244b684769d36ed0dd1c37f
vantohuu~ <vantohuu1234567@gmail.com>
2024-05-10 10:05:04 +0700

    done

--- backend/mockdata/booking.js
+++ backend/mockdata/booking.js
@@ -10,8 +10,8 @@ bookingAction.create = (booking) =>
 bookingAction.delete = (id) => 
 {
     const index = listBooking.findIndex((e) => e.id == id);
-    // console.log(index);
-    if (index > 0 && index < listBooking.length) listBooking.splice(index, 1);
+    console.log(index);
+    if (index >=0 && index < listBooking.length) listBooking.splice(index, 1);
 }
 
 bookingAction.getAll = () => listBooking;
